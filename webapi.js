0f9c80dd579131c09f0e00000480d7c69b8dd09f
Tulsipada <Tulsipada55@gmail.com>
2022-02-20 23:13:17 +0530

    Add buy sell function

--- webapi.js
+++ webapi.js
@@ -2,30 +2,28 @@
     require("dotenv").config(); //env
    var price,highprice = 0 ;
     const ws = new WebSocket(process.env.API)
-    //const ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');
+
+
+
+    const coinbuy = function(){
+      console.log("buy");
+  }
+  const coinsell = function(){
+      console.log("sell");
+  }
+
     ws.onmessage = even =>{
-      price = JSON.parse(even.data).p; 
+      price = JSON.parse(even.data).c; 
       high = JSON.parse(even.data).h; 
       low = JSON.parse(even.data).l; 
-  //     if (price < highprice){
-  //         highprice = price;
-  //         console.log(highprice);
-  //       coinbuy()
-  //     }else{
-  //       highprice = price;
-  //       console.log(highprice);
-  //       coinsell()
-  //     }  
-  //  }
-
-  //   const coinbuy = function(){
-  //       console.log("buy");
-  //   }
-  //   const coinsell = function(){
-  //       console.log("sell");
-  //   }
-
-  console.log("high price"+ high);
-  console.log("low price" + low);
-
-    }
+      if (price < highprice){
+          highprice = price;
+          console.log(highprice);
+        coinbuy()
+      }else{
+        highprice = price;
+        console.log(highprice);
+        coinsell()
+      }  
+  console.log("price"+ price);
+}
